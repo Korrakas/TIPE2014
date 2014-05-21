d0d38f6e4d73526b2646d280aaf7ba1ac6f89710
Korrakas <delaveau.lilian@orange.fr>
2014-05-21 13:26:47 +0100

    Create ttgraphique.ml

---
+++ ttgraphique.ml
@@ -0,0 +1,7 @@
+(* Fichier ttgraphique.ml : librairie graphique adaptée à la représentation de graphes sous forme de tableaux, listes, etc.*)
+#open "graphics";;
+open_graph "";;
+
+let trace_cadre tx ty pasx pasy =
+  set_color 0x000000;
+  
